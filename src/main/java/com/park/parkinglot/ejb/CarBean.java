9dcc3622bfe70ddc3eb0ff16b73ff96ca0317444
Florescu Andrei <andrei.florescu.00@gmail.com>
2020-11-16 11:53:43 +0200

    Car CRUD

--- src/main/java/com/park/parkinglot/ejb/CarBean.java
+++ src/main/java/com/park/parkinglot/ejb/CarBean.java
@@ -7,6 +7,7 @@ package com.park.parkinglot.ejb;
 
 import com.park.parkinglot.common.CarDetails;
 import com.park.parkinglot.entity.Car;
+import com.park.parkinglot.entity.User;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
@@ -36,13 +37,53 @@ public class CarBean {
             throw new EJBException(ex);
         }
     }
-    
-    private List<CarDetails> copyCarsToDetails(List<Car> cars){
+
+    private List<CarDetails> copyCarsToDetails(List<Car> cars) {
         List<CarDetails> detailsList = new ArrayList<>();
-        for(Car car:cars){
-            CarDetails carDetails = new CarDetails(car.getId(), car.getLicensePlate(), car.getParkingSpot(),car.getUser().getUsername());
+        for (Car car : cars) {
+            CarDetails carDetails = new CarDetails(car.getId(), car.getLicensePlate(), car.getParkingSpot(), car.getUser().getUsername());
             detailsList.add(carDetails);
         }
         return detailsList;
+    }
+
+    public void createCar(String licensePlate, String parkingSpot, Integer userId) {
+        LOG.info("createCar");
+        Car car = new Car();
+        car.setLicensePlate(licensePlate);
+        car.setParkingSpot(parkingSpot);
+
+        User user = em.find(User.class, userId);
+        user.getCars().add(car);
+        car.setUser(user);
+
+        em.persist(car);
+    }
+
+    public CarDetails findById(Integer carId) {
+        Car car = em.find(Car.class, carId);
+        return new CarDetails(car.getId(), car.getLicensePlate(), car.getParkingSpot(), car.getUser().getUsername());
+    }
+
+    public void updateCar(Integer carId, String licensePlate, String parkingSpot, Integer userId) {
+        LOG.info("updateCar");
+        Car car = em.find(Car.class, carId);
+        car.setLicensePlate(licensePlate);
+        car.setParkingSpot(parkingSpot);
+
+        User oldUser = em.find(User.class, userId);
+        oldUser.getCars().remove(car);
+
+        User user = em.find(User.class, userId);
+        user.getCars().add(car);
+        car.setUser(user);
+    }
+
+    public void deleteCarsByIds(List<Integer> carIds) {
+        LOG.info("deleteCarsByIds");
+        for (Integer id : carIds) {
+            Car car = em.find(Car.class, id);
+            em.remove(car);
+        }
     }
 }
--- src/main/java/com/park/parkinglot/servlet/EditCar.java
+++ src/main/java/com/park/parkinglot/servlet/EditCar.java
@@ -6,10 +6,11 @@
 package com.park.parkinglot.servlet;
 
 import com.park.parkinglot.common.CarDetails;
+import com.park.parkinglot.common.UserDetails;
 import com.park.parkinglot.ejb.CarBean;
+import com.park.parkinglot.ejb.UserBean;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
 import java.util.List;
 import javax.inject.Inject;
 import javax.servlet.ServletException;
@@ -22,8 +23,14 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author andrei
  */
-@WebServlet(name = "Cars", urlPatterns = {"/Cars"})
-public class Cars extends HttpServlet {
+@WebServlet(name = "EditCar", urlPatterns = {"/EditCar"})
+public class EditCar extends HttpServlet {
+
+    @Inject
+    UserBean userBean;
+
+    @Inject
+    CarBean carBean;
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -34,9 +41,6 @@ public class Cars extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    @Inject
-    private CarBean carBean;
-
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
@@ -45,10 +49,10 @@ public class Cars extends HttpServlet {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet Cars</title>");
+            out.println("<title>Servlet EditCar</title>");
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet Cars at " + request.getContextPath() + "</h1>");
+            out.println("<h1>Servlet EditCar at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
@@ -66,13 +70,14 @@ public class Cars extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        request.setAttribute("activePage", "Cars");
-        request.setAttribute("numberOfFreeParkingSpots", 10);
+        List<UserDetails> users = userBean.getAllUsers();
 
-        List<CarDetails> cars = carBean.getAllCars();
-        request.setAttribute("cars", cars);
+        int carId = Integer.parseInt(request.getParameter("id"));
+        CarDetails car = carBean.findById(carId);
+        request.setAttribute("car", car);
 
-        request.getRequestDispatcher("/WEB-INF/pages/cars.jsp").forward(request, response);
+        request.setAttribute("users", users);
+        request.getRequestDispatcher("/WEB-INF/pages/editCar.jsp").forward(request, response);
     }
 
     /**
@@ -86,14 +91,12 @@ public class Cars extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        String[] carIdsString = request.getParameterValues("car_ids");
-        if (carIdsString != null) {
-            List<Integer> carIds = new ArrayList<>();
-            for (String id : carIdsString) {
-                carIds.add(Integer.parseInt(id));
-            }
-            carBean.deleteCarsByIds(carIds);
-        }
+        String licensePlate = request.getParameter("license_plate");
+        String parkingSpot = request.getParameter("parking_spot");
+        int ownerId = Integer.parseInt(request.getParameter("owner_id"));
+        int carId = Integer.parseInt(request.getParameter("car_id"));
+        carBean.updateCar(carId, licensePlate, parkingSpot, ownerId);
+
         response.sendRedirect(request.getContextPath() + "/Cars");
     }
 
